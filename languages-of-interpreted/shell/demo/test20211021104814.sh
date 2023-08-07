164ee1b1b974df95c8e4a373dcba81284f982e7d
Wenger Binning <wengerbinnning@163.com>
2023-08-07 14:50:20 +0800

    branch(old/master): remove shell

--- languages-of-interpreted/shell/demo/test20211021104814.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DEBUG=1
-
-[ -n "$DEBUG" ] && echo "DEBUG" || echo "RELEASE"
-
-
--- languages-of-interpreted/shell/demo/testpath.sh
+++
@@ -1,9 +0,0 @@
-# This is script file's path & name.
-SCRIPT_PATH="${0%/*}"
-SCRIPT_NAME="${0##*/}"
-echo script path:${SCRIPT_PATH} ,script name:$SCRIPT_NAME
-
-# This is environment variable about recording work path.
-echo ${PWD}
-
-
